b3e675e6cf6375a8479a359a1cae65d89a670b80
Dev-Madhan <maddyclickz24@gmail.com>
2026-08-20 22:14:16 +0530

    Better Auth half installed & Deployment error fix

--- lib/env.ts
+++ lib/env.ts
@@ -11,6 +11,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    BETTER_AUTH_API_KEY: z.string().min(1),
   },
 
   /*
@@ -27,6 +28,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    BETTER_AUTH_API_KEY: process.env.BETTER_AUTH_API_KEY,
     // NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
   
